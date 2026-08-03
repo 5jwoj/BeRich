f9215486aca66e835ed75bea0a9ed1227eca9697
ONNO <32419149+5jwoj@users.noreply.github.com>
2026-08-03 10:47:50 +0800

    Update jd_log_monitor.js

--- JDCK/jd_log_monitor.js
+++ JDCK/jd_log_monitor.js
@@ -11,7 +11,7 @@
  * 
  * --------------------------------------------------------------------------------
  * QX 任务配置 (task_local):
- * 0 0-23/1 * * * https://raw.githubusercontent.com/5jwoj/BeRich/main/JDCK/jd_log_monitor.js, tag=日志数值检测通知, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+ * 0 0-23/1 * * * https://raw.githubusercontent.com/5jwoj/BeRich/main/JDCK/jd_log_monitor.js, tag=充值金检测通知, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
  * --------------------------------------------------------------------------------
  */
 
